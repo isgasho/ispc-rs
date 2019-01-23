26cac5e3b58e74cdcec2758946fe4363b2c2ec6f
Will Usher <willusher.life@gmail.com>
2019-01-23 10:19:00 -0700

    Trying out circleci

--- .circleci/build-ispc-rs-linux.sh
+++ .circleci/build-ispc-rs-linux.sh
@@ -2,12 +2,11 @@
 
 set -x
 
-sudo apt-get update
-sudo apt-get install build-essential -y
-
 echo "Setting up Rust"
-curl https://sh.rustup.rs -sSf | sh
-export PATH=$PATH:~/.cargo/bin/
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
+rustc --version
+cargo --version
 
 export LLVM_VERSION=5.0.2
 echo "Setting up LLVM ${LLVM_VERSION}"
@@ -18,8 +17,28 @@ tar -xf ${LLVM}.tar.xz -C llvm --strip-components=1
 export LIBCLANG_PATH=`pwd`/llvm/lib/
 
 echo "Setting up ISPC"
-wget -O ispc.tar.gz https://downloads.sourceforge.net/project/ispcmirror/v1.10.0/ispc-v1.10.0-${TRAVIS_OS_NAME}.tar.gz
+wget -O ispc.tar.gz https://downloads.sourceforge.net/project/ispcmirror/v1.10.0/ispc-v1.10.0-linux.tar.gz
 tar -xvf ispc.tar.gz
 export PATH=$PATH:`pwd`/ispc-1.10.0-Linux/bin/:`pwd`/ispc-1.10.0-Darwin/bin/
 ispc --version
+
+cargo build
+cargo test
+cargo doc
+
+echo "Building examples"
+
+for d in `ls examples/`; do
+		cd examples/${d}/
+		pwd
+		if [[ "$d" == "simple" ]]; then
+				cargo build --features ispc
+		fi
+		cargo build
+		if [[ "$?" != "0" ]]; then
+				exit 1
+		fi
+		cd ../../
+done
+
 
